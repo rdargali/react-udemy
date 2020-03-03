134831d295d4a0de2cd446127f6c7350b32c97f0
Rawand Dargali <rdargali@yahoo.com>
2020-03-03 00:22:39 -0600

    second commit

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
   render() {
-    return <div className="App"></div>;
+    return (
+      <div className="App">
+        <h1>hi im a react app</h1>
+        <p>im re learning react</p>
+      </div>
+    );
   }
 }
 
