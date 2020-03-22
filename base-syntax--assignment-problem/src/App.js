19603bc57130ca9c4d07d1be8a997b3f4113e427
Rawand Dargali <rdargali@yahoo.com>
2020-03-21 19:31:56 -0500

    chap 3 exercise completed

--- base-syntax--assignment-problem/src/App.js
+++ base-syntax--assignment-problem/src/App.js
@@ -1,22 +1,74 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+
+import UserOutput from "./components/UserOutput";
+import UserInput from "./components/UserInput";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: "maximillian"
+    };
+  }
+
+  changeNameHandler = () => {
+    this.setState({
+      username: "tim duncan"
+    });
+  };
+
   render() {
     return (
       <div className="App">
         <ol>
           <li>Create TWO new components: UserInput and UserOutput</li>
-          <li>UserInput should hold an input element, UserOutput two paragraphs</li>
-          <li>Output multiple UserOutput components in the App component (any paragraph texts of your choice)</li>
-          <li>Pass a username (of your choice) to UserOutput via props and display it there</li>
-          <li>Add state to the App component (=> the username) and pass the username to the UserOutput component</li>
-          <li>Add a method to manipulate the state (=> an event-handler method)</li>
-          <li>Pass the event-handler method reference to the UserInput component and bind it to the input-change event</li>
-          <li>Ensure that the new input entered by the user overwrites the old username passed to UserOutput</li>
-          <li>Add two-way-binding to your input (in UserInput) to also display the starting username</li>
-          <li>Add styling of your choice to your components/ elements in the components - both with inline styles and stylesheets</li>
+          <li>
+            UserInput should hold an input element, UserOutput two paragraphs
+          </li>
+          <li>
+            Output multiple UserOutput components in the App component (any
+            paragraph texts of your choice)
+          </li>
+          <li>
+            Pass a username (of your choice) to UserOutput via props and display
+            it there
+          </li>
+          <li>
+            Add state to the App component (=> the username) and pass the
+            username to the UserOutput component
+          </li>
+          <li>
+            Add a method to manipulate the state (=> an event-handler method)
+          </li>
+          <li>
+            Pass the event-handler method reference to the UserInput component
+            and bind it to the input-change event
+          </li>
+          <li>
+            Ensure that the new input entered by the user overwrites the old
+            username passed to UserOutput
+          </li>
+          <li>
+            Add two-way-binding to your input (in UserInput) to also display the
+            starting username
+          </li>
+          <li>
+            Add styling of your choice to your components/ elements in the
+            components - both with inline styles and stylesheets
+          </li>
         </ol>
+        <br />
+        <br />
+        <br />
+
+        <button onClick={this.changeNameHandler}>Change name!</button>
+
+        <UserInput />
+        <UserOutput username="rawand" />
+        <UserOutput username="nujeen" />
+        <UserOutput username={this.state.username} />
       </div>
     );
   }
---
+++ base-syntax--assignment-problem/src/components/UserOutput.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function UserOutput(props) {
+  return (
+    <div>
+      <p>my name is {props.username}</p>
+      <p>paragraph 2</p>
+    </div>
+  );
+}
